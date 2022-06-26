c9ee100ab997efce486e80e557158c1a4108c1fd
Hamid Aebadi <hamidebadi1996@yahoo.com>
2022-06-26 13:51:24 +0300

    login implemented in backend

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,30 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('../models/users')
+const config = require('../utils/config')
 const loginRouter = require('express').Router()
 
-loginRouter.post('/', (req, res) => {
-    
+loginRouter.post('/', async(req, res) => {
+    const {email, password} = req.body
+    const user = await User.findOne({email: email})
+    const passwordCorrect = user === null
+        ? false
+        : await bcrypt.compare(password, user.passwordHash)
+
+    if(!(user && passwordCorrect)){
+        return res.status(401).json({
+            error: "invalid username or password"
+        })
+    }
+
+    const userForToken = {
+        email: user.email,
+        id: user._id
+    }
+
+    const token = jwt.sign(userForToken, config.SECRET)
+    res.status(200)
+    .send({token, email: user.email, firstName: user.firstName})
 })
 
 module.exports = loginRouter
